75b979bd30315e2eca4fb346dba4f35a6df98492
AWang-Cabin <dbdxwyn@163.com>
2021-10-29 15:25:21 -0400

    lab4 draft

---
+++ lab4/build/CMakeFiles/lab4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab4.dir/Sockets.cpp.o"
+  "CMakeFiles/lab4.dir/TCP.cpp.o"
+  "CMakeFiles/lab4.dir/UDP.cpp.o"
+  "lab4.pdb"
+  "lab4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
